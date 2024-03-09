ab63d651fbc3fd48fabf822d36b29fb88dfe567f
kaleab <kaleabtadesse49@gmail.com>
2024-03-09 13:22:49 +0300

    styled send input component

--- AppComponents/SendInputComponent.js
+++ AppComponents/SendInputComponent.js
@@ -8,16 +8,13 @@ const SendInputComponent =({placeholder,icon,style,sendOnPress,...other}) => {
   return (
     <View style = {[styles.container,style]} >
         <MaterialCommunityIcons color="#696969" style ={styles.icon} name = {icon} size={24}/>
-            <TextInput 
-            multiline  
-            style ={[styles.textarea]} 
+            <TextInput
+            multiline
+            style ={[styles.textarea]}
             placeholder={placeholder} {...other} />
         <TouchableWithoutFeedback onPress={sendOnPress}>
-
             <FontAwesome name='send' size={24} style = {styles.sendIcon} />
         </TouchableWithoutFeedback>
-
-  
     </View>
   )
 }
@@ -26,24 +23,32 @@ export default SendInputComponent
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor:color.smoke,
+        backgroundColor: "white",
         flexDirection:'row',
+        alignItems: "center",
         margin:4,
         marginHorizontal:10,
-        borderRadius:20
+        borderRadius:20,
+        elevation: 5,
+        shadowColor: '#171717',
+        shadowOffset: {width: -2, height: 4},
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
     },textarea:{
         fontSize:16,
         margin:4,
         flex:1,
-        textAlignVertical:'top'
     },icon:{
         marginLeft:15,
         alignSelf:'flex-end',
         marginBottom:5
-        
+
     },sendIcon:{
         alignSelf:'flex-end',
         marginRight:10,
-        marginBottom:5
+        marginBottom:6,
+        // backgroundColor: "#bbb",
+        // padding: 8,
+        // borderRadius: 24
     }
 })
